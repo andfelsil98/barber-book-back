978bacd14383eb56a8a4128e3c055c1beeb15a88
Andres Felipe Silva Moreno <andfelsil98@gmail.com>
2026-03-31 11:27:00 -0500

    changes

--- src/config/cloud-tasks.config.ts
+++ src/config/cloud-tasks.config.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_SEND_MESSAGE_PATH = "/api/whatsapp/send-message";
+export const WHATSAPP_SEND_MESSAGE_PATH = "/whatsapp/send-message";
 export const CLOUD_TASK_TOKEN_HEADER = "x-internal-task-token";
